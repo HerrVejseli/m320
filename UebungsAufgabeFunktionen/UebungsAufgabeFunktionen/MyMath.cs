04c78141c9d31349fb74e4da09c149f9aa00f7b9
Visar Vejseli <visarvejseli06@gmail.com>
2025-08-25 09:14:04 +0200

    alles bis rekursiv

---
+++ UebungsAufgabeFunktionen/UebungsAufgabeFunktionen/MyMath.cs
@@ -0,0 +1,35 @@
+﻿namespace UebungsAufgabeFunktionen
+{
+    internal class MyMath
+    {
+        public static int Calc_ggT(int a, int b)
+        {
+            int z ;
+            while (b != 0)
+            {
+                z = a % b; 
+                a = b;        
+                b = z;     
+            }
+            return a; 
+        }
+        public static void ShowResult(int ersteZahl, int zweiteZahl, int ggT, int kgV)
+        {
+            Console.WriteLine($"ggT von {ersteZahl} und {zweiteZahl} ist {ggT}");
+            Console.WriteLine($"kgV von {ersteZahl} und {zweiteZahl} ist {kgV}");
+        }
+
+
+
+        public static int Calc_kgV(int a, int b)
+        {
+            int ggT = Calc_ggT(a, b);
+
+            int x = a * b;
+
+            int kgV = x / ggT;
+
+            return kgV;
+        }
+    }
+}
---
+++ UebungsAufgabeFunktionen/UebungsAufgabeFunktionen/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+namespace UebungsAufgabeFunktionen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool weiter = true;
+
+            while (weiter)
+            {
+                int ersteZahl = 0;
+                int zweiteZahl = 0;
+
+                Console.WriteLine("Gib die erste Zahl ein mein Sohn");
+
+                if (int.TryParse(Console.ReadLine(), out ersteZahl))
+                {
+                    Console.WriteLine("Gib die zweite Zahl ein mein Sohn");
+                    if (int.TryParse(Console.ReadLine(), out zweiteZahl))
+                    {
+                        int ggT = MyMath.Calc_ggT(ersteZahl, zweiteZahl);
+                        int kgV = MyMath.Calc_kgV(ersteZahl, zweiteZahl);
+
+                        MyMath.ShowResult(ersteZahl, zweiteZahl, ggT, kgV);
+
+                        
+                    }
+                }
+
+               
+                Console.WriteLine("Willst du noch eine Berechnung machen? (j/n)");
+                string antwort = Console.ReadLine().ToLower();
+
+                if (antwort != "j")
+                {
+                    weiter = false;
+                }
+
+                
+            }
+
+            Console.WriteLine("Programm beendet. Drücke eine Taste zum Schließen.");
+            Console.ReadKey();
+        }
+    }
+}
